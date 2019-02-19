7db309727665bf9c0f5e023e44a0aba5999074f7
Meshal Al-zahrni <sh3lool@hotmail.com>
2019-02-19 18:30:24 +0300

    MISHAL90

--- api/serializers.py
+++ api/serializers.py
@@ -9,3 +9,13 @@ class RestaurantListSerializer(serializers.ModelSerializer):
         	'opening_time',
         	'closing_time',
         	]
+
+class RestaurantDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['id', 'owner', 'name', 'description', 'opening_time', 'closing_time']
+
+class RestaurantUpdatelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['id', 'description', 'opening_time', 'closing_time']
--- api/views.py
+++ api/views.py
@@ -1,20 +1,30 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView ,RetrieveUpdateAPIView, RetrieveAPIView,DestroyAPIView
 from restaurants.models import Restaurant
-from .serializers import RestaurantListSerializer
+from .serializers import RestaurantListSerializer,RestaurantDetailSerializer , RestaurantUpdatelSerializer
 
 class RestaurantListView(ListAPIView):
-	queryset = Restaurant.objects.all()
-	serializer_class = RestaurantListSerializer
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantListSerializer
 
 # Complete me
-class RestaurantDetailView():
+class RestaurantDetailView(RetrieveAPIView):
 
-
-
-# Complete me
-class RestaurantUpdateView():
-
-
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantDetailSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'restaurant_id'
 
 # Complete me
-class RestaurantDeleteView():
+class RestaurantUpdateView(RetrieveUpdateAPIView):
+
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantUpdatelSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'restaurant_id'
+
+# Complete me
+class RestaurantDeleteView(DestroyAPIView):
+
+    queryset = Restaurant.objects.all()
+    lookup_field = 'id'
+    lookup_url_kwarg = 'restaurant_id'
